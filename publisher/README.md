44b4e8284465567941f931c07bfcdbb50b81b74a
Jelle van der Waa <jelle@vdwaa.nl>
2016-09-18 01:47:43 +0200

    Initial commit

---
+++ publisher/README.md
@@ -0,0 +1,14 @@
+Weather forecast to MQTT bridge
+-------------------------------
+
+Pushes weather forecast information from the openweathermap to MQTT.
+See the following url for the forecast information http://openweathermap.org/forecast5
+
+
+Requirements
+-------------
+
+The following Python libraries are required.
+
+* paho-mqtt
+* requests
---
+++ publisher/weather_to_mqtt.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import requests
+import paho.mqtt.publish as publish
+
+from datetime import datetime
+
+def kelvin_to_celcius(k):
+    return k - 273.15
+
+def publish_weather(prefix, data, forecast = False):
+    main = data['main']
+    wind = data['wind']
+    weather = data['weather'][0]
+    clouds = data['clouds']['all']
+    rain = data['rain']['3h'] if data['rain'] else 0
+
+    msgs = [
+            {"topic": prefix + "humidity", "payload": "%s %%" % main['humidity'], "retain": True},
+            {"topic": prefix + "pressure", "payload": "%s hPa" % main['pressure'], "retain": True},
+            {"topic": prefix + "temperature", "payload": u"%s \xb0C" % kelvin_to_celcius(main['temp']), "retain": True},
+            {"topic": prefix + "temperature_min", "payload": u"%s \xb0C" % kelvin_to_celcius(main['temp_min']), "retain": True},
+            {"topic": prefix + "temperature_max", "payload": u"%s \xb0C" % kelvin_to_celcius(main['temp_max']), "retain": True},
+            {"topic": prefix + "type", "payload": weather['main'], "retain": True},
+            {"topic": prefix + "description", "payload": weather['description'], "retain": True},
+            # http://openweathermap.org/weather-conditions
+            {"topic": prefix + "code", "payload": weather['id'], "retain": True},
+            {"topic": prefix + "wind_direction", "payload": u"%s \xb0C" % wind['deg'], "retain": True},
+            {"topic": prefix + "wind_speed", "payload": "%s m/s" % wind['speed'], "retain": True},
+            {"topic": prefix + "rain", "payload": "%s mm" % rain, "retain": True},
+            {"topic": prefix + "clouds", "payload": "%s %%" % clouds, "retain": True},
+         ]
+
+
+    if forecast: # Unique to normal req
+        # Fetch sunset/sunrise
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s' % (CITY, APPKEY))
+
+        data = r.json()
+        sys = data['sys']
+
+        sunset = datetime.fromtimestamp(sys['sunset'])
+        sunrise = datetime.fromtimestamp(sys['sunrise'])
+        msgs.append({"topic": prefix + "sunrise", "payload": sunset.isoformat(), "retain": True})
+        msgs.append({"topic": prefix + "sunset", "payload": sunrise.isoformat(), "retain": True})
+
+    publish.multiple(msgs, hostname=SERVER)
+
+APPKEY = ''
+CITY = 'The Hague'
+SERVER = ''
+
+# api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml
+r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q=%s&APPID=%s' % (CITY, APPKEY))
+data = r.json()
+
+# Get the first two hours
+for index, data in enumerate(data['list'][0:2]):
+    if index == 0:
+        prefix = 'revspace/weather/'
+        publish_weather(prefix, data)
+
+    if index == 1:
+        prefix = 'revspace/weather/forecast/'
+        publish_weather(prefix, data, True)
